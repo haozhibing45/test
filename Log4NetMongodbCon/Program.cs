d1b98f7d1b2936d811e18c34a4cfdbe6037345b5
hader hao <haderhao45@outlook.com>
2018-12-05 17:27:42 +0800

    测试

--- Log4NetMongodbCon/Program.cs
+++ Log4NetMongodbCon/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Reflection;
 using log4net;
+using TestMogonDbCURD;
 
 namespace Log4NetMongodbCon
 {
@@ -7,40 +10,57 @@ namespace Log4NetMongodbCon
     {
         static void Main(string[] args)
         {
-      
-            MongoDbHelper<LogInfo> mg = new MongoDbHelper<LogInfo>();
-           
-            for (int i = 0; i < 10; i++)
-            {
-                LogInfo info = new LogInfo
-                {
-                    UserName = "admin",
-                    UserType = i.ToString(),
-                    Status = "2",
-                    FailPwd = "FailPwd",
-                    LogIp = "123",
-                    LogIpLocation = "1325",
-                    UserAgent =
-                        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36",
-                    Referrer = "www.baidu.com"
-                };
-                mg.Insert(info);
-            }
 
-            var list = mg.QueryAll();
-            foreach (var item in list)
-            {
-                Console.WriteLine(item.Id);
-            }
+            info();
+           
           
             //            ILog log = new MongodbLog().RetLog(info);
             //            log.Info("测试");
             //            Console.Write(log.Logger.Name+"........");
             Console.ReadLine();
         }
-    }
+        public static void info()
+        {
 
-    internal class LogInfo:BaseEntity
+            MongoDbHelper<LogInfoTest> mg = new MongoDbHelper<LogInfoTest>();
+
+            for (int i = 0; i < 2; i++)
+            {
+                StackTrace ss = new StackTrace(true);
+                //index:0为本身的方法；1为调用方法；2为其上上层，依次类推
+                MethodBase mb = ss.GetFrame(1).GetMethod();
+                string systemModule = Environment.NewLine;
+                systemModule += "模块名:" + mb.Module + Environment.NewLine;
+                systemModule += "命名空间名:" + mb.ReflectedType.Namespace + Environment.NewLine;
+                //完全限定名，包括命名空间
+                systemModule += "类名:" + mb.ReflectedType.FullName + Environment.NewLine;
+                systemModule += "方法名:" + mb.Name;
+                StackFrame[] sfs = ss.GetFrames();
+                LogInfoTest info = new LogInfoTest
+                {
+                    UserName = "admin",
+                    UserType = i.ToString(),
+                    Status = "2",
+                    FailPwd = systemModule,
+                    LogIp = "123",
+                    LogIpLocation = "1325",
+                    UserAgent =
+                        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36",
+                    Referrer = null
+                };
+                mg.Insert(info);
+              
+            }
+            var list = mg.QueryAll();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.Id);
+            }
+
+        }
+
+    }
+    internal class LogInfoTest:BaseEntity
     {
         public string UserId { get; set; }
         public string UserName { get; set; }
---
+++ TestMogonDbCURD/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestMogonDbCURD
+{
+    public class BaseEntity
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public string State { get; set; }
+
+        public string CreateTime { get; set; }
+
+        public string UpdateTime { get; set; }
+    }
+}
